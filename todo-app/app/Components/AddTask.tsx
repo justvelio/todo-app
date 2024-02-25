fb031c711206db5b2edc0fbf22456784b10f770a
Velislav Kostadinov <velislav.kostadinov222@gmail.com>
2024-02-25 19:46:24 +0200

    create button for adding new tasks

---
+++ todo-app/app/Components/AddTask.tsx
@@ -0,0 +1,10 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
+export default function AddTask() {
+    return (
+        <div>
+            <button className="btn btn-primary w-full border-none text-center font-bold">NEW TASK <AiOutlinePlus className="ml-1 mb-0.5" size={17}/></button>
+        </div>
+
+    )
+}
--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -1,7 +1,14 @@
+import AddTask from "./Components/AddTask";
+import TodoList from "./Components/TodoList";
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-center">TODO App</h1>
+    <main className="max-w-4xl mx-auto">
+      <div className="text-center my-5 flex flex-col gap-4">
+        <h1 className="font-bold text-3xl text-cyan-600">Todo App</h1>
+        <AddTask />
+      </div>
+      <TodoList />
     </main>
   )
 }
