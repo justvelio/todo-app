7cc52acd90b1d7203a5e1a0efb9dc1c0acf0ee67
Velislav Kostadinov <velislav.kostadinov222@gmail.com>
2024-02-25 19:58:43 +0200

    init json-server; create todo list skeleton;

--- todo-app/app/Components/TodoList.tsx
+++ todo-app/app/Components/TodoList.tsx
@@ -1,7 +1,22 @@
 export default function TodoList() {
     return (
         <div>
-            todolist functionality
+            <div className="overflow-x-auto">
+  <table className="table">
+    <thead>
+      <tr className="text-lg">
+        <th>Name</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr className="hover">
+        <td>Cy Ganderton</td>
+        <td>Blue</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
         </div>
     )
 }
