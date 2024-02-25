95cd8ede7aa62d1339eb1707b92de16eed0e27bf
Velislav Kostadinov <velislav.kostadinov222@gmail.com>
2024-02-25 20:10:05 +0200

    connect json-server

--- todo-app/app/Components/TodoList.tsx
+++ todo-app/app/Components/TodoList.tsx
@@ -1,22 +1,33 @@
-export default function TodoList() {
+import { ITask } from "@/types/tasks"
+
+interface TodoListProps {
+    tasks: ITask[]
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
     return (
         <div>
             <div className="overflow-x-auto">
   <table className="table">
     <thead>
       <tr className="text-lg">
-        <th>Name</th>
+        <th>Task</th>
         <th>Favorite Color</th>
       </tr>
     </thead>
     <tbody>
-      <tr className="hover">
-        <td>Cy Ganderton</td>
+        {tasks.map(task => (
+
+            <tr key={task.id} className="hover">
+        <td>{task.text}</td>
         <td>Blue</td>
       </tr>
+          ))}
     </tbody>
   </table>
 </div>
         </div>
     )
 }
+
+export default TodoList;
--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -1,14 +1,17 @@
+import { getTodos } from "@/api";
 import AddTask from "./Components/AddTask";
 import TodoList from "./Components/TodoList";
 
-export default function Home() {
+export default async function Home() {
+const tasks = await getTodos();
+
   return (
     <main className="max-w-4xl mx-auto">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="font-bold text-3xl text-cyan-600">Todo App</h1>
         <AddTask />
       </div>
-      <TodoList />
+      <TodoList tasks={tasks} />
     </main>
   )
 }
